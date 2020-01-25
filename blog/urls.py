7221561f67ee65f0e151962ab323cb9c240d67dc
djadar <mukamaugusta5@gmail.com>
2020-01-24 22:47:51 -0800

    Ajout de vues qui permettent de créer et d'éditer un post de blog sur le site

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,10 @@ from . import views
 app_name = 'blog'
 urlpatterns = [
     path('',views.post_list, name='post_list'),
-    path('post/<int:pk>/',views.post_detail, name='post_detail')
+    path('post/<int:pk>/',views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/',views.post_edit, name='post_edit'),
+    
     #path('^post/(?P<pk>[0-9]+)/$/',views.post_detail, name='post_detail'),
 
 ]
